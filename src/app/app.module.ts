ab917b436b913aa4ab2f2566e72e102baf88c9d8
kshitij khanal <k.kshitij.k.khanal@gmail.com>
2023-03-26 19:25:25 +0545

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {CKEditorModule} from 'ng2-ckeditor';
+import {FormsModule} from "@angular/forms";
+import { CkeditorPopupComponent } from './ckeditor-popup/ckeditor-popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CkeditorPopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
