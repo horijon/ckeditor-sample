8f3926d6c81a49c5a69ffaa95147d89e76aed782
kshitij khanal <k.kshitij.k.khanal@gmail.com>
2023-03-27 07:47:56 +0545

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {CKEditorModule} from 'ng2-ckeditor';
 import {FormsModule} from "@angular/forms";
-import { CkeditorPopupComponent } from './ckeditor-popup/ckeditor-popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CkeditorPopupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
